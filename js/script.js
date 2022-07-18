d93ac29604a765a2c4de763cea28474308121dad
Alexey Anasov <webdev.alexey94@yandex.by>
2022-07-18 17:43:20 +0300

    add homework1 +bonus

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const nameOfProduct = 'Hyundai H-SF16-F1601'; // Наименование товара
+const quantityOfGoods = 5; // Количество товара
+const productCategory = 'Вентилятор'; // Категория товара
+const priceProduct = 89; // Цена товара
+
+console.log('Наименование товара: ', nameOfProduct);
+
+let totalAmount = quantityOfGoods * priceProduct;
+console.log('Общая сумма:', totalAmount);
+
